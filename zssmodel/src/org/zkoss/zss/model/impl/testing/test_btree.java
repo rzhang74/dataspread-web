1cb728de44998280d24a33ceefe9d4f495ed2024
stanzhou24 <stanzhou24@gmail.com>
2017-05-31 14:50:47 -0500

    now

--- zssmodel/src/org/zkoss/zss/model/impl/testing/test_btree.java
+++ zssmodel/src/org/zkoss/zss/model/impl/testing/test_btree.java
@@ -2,6 +2,8 @@ package org.zkoss.zss.model.impl.testing;
 
 import org.model.BlockStore;
 import org.model.DBContext;
+import org.model.DBHandler;
+import org.zkoss.zss.model.impl.BTree;
 import org.zkoss.util.logging.Log;
 import org.postgresql.copy.CopyIn;
 import org.postgresql.copy.CopyManager;
@@ -11,6 +13,11 @@ import org.postgresql.jdbc.PgConnection;
  */
 public class test_btree  {
     public static void main(String []args){
+        String tableName = "Test";
+        DBHandler connection = new DBHandler();
+        DBContext context = new DBContext(connection.getConnection());
+        BTree testTree = new BTree(context, tableName);
+        Integer [] ids = testTree.createIDs(context, 0 , 10);
 
     }
 }
